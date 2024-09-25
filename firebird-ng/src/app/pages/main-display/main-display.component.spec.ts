c4a83d4c12f1ec51d8e65cedf69fc0d0cdda5b27
Dmitry Romanov <romanovda@gmail.com>
2024-09-25 10:50:48 -0400

    [ng] Fix unit tests

--- firebird-ng/src/app/pages/main-display/main-display.component.spec.ts
+++ firebird-ng/src/app/pages/main-display/main-display.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MainDisplayComponent } from './main-display.component';
 import { provideRouter } from '@angular/router';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('MainDisplayComponent', () => {
   let component: MainDisplayComponent;
@@ -8,7 +9,7 @@ describe('MainDisplayComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MainDisplayComponent],
+      imports: [MainDisplayComponent, HttpClientTestingModule,],
       providers: [provideRouter([])],
     })
     .compileComponents();
