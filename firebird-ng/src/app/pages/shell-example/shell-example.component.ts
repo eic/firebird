9a03570a0180a14dbc0262c1b6cd119b65541926
Dmitry Romanov <romanovda@gmail.com>
2024-10-05 00:59:23 -0400

    Shell component added

---
+++ firebird-ng/src/app/pages/shell-example/shell-example.component.ts
@@ -0,0 +1,26 @@
+import {Component, ViewChild} from '@angular/core';
+import {DisplayShellComponent} from "../../components/display-shell/display-shell.component";
+
+@Component({
+  selector: 'app-shell-example',
+  standalone: true,
+  imports: [
+    DisplayShellComponent
+  ],
+  templateUrl: './shell-example.component.html',
+  styleUrl: './shell-example.component.scss'
+})
+export class ShellExampleComponent {
+
+  @ViewChild(DisplayShellComponent)
+  displayShellComponent!: DisplayShellComponent;
+
+  toggleLeftPane() {
+    this.displayShellComponent.toggleLeftPane();
+  }
+
+  toggleRightPane() {
+    this.displayShellComponent.toggleRightPane();
+  }
+
+}
